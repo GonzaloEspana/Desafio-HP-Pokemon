1b2cf52ea72281677447e071dd84a6e36c1309d0
Gonzalo España <gespana@alumnos.uai.cl>
2022-12-05 10:03:38 -0300

    link fixed

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -6,13 +6,13 @@ export function Navbar() {
       <header className="">
         <nav className="navbar navbar-expand-lg border-bottom border-light shadow-sm navbar-light ">
           <div className="container lora fw-bolder justify-content-center">
-            <a className="navbar-brand" href="">
+            <span className="navbar-brand" href="">
               <img
                 className="logo"
                 src={require("../images/logo_poke.png")}
                 alt=""
               />
-            </a>
+            </span>
           </div>
         </nav>
       </header>
